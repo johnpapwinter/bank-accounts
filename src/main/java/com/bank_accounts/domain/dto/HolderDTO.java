e1807dea0ecf762763dae8bfbf7bc8510a18e4a1
John Papachimonas <105006776+johnpapwinter@users.noreply.github.com>
2023-04-10 22:44:30 +0300

    refactor accounts and holders dtos

--- src/main/java/com/bank_accounts/domain/dto/HolderDTO.java
+++ src/main/java/com/bank_accounts/domain/dto/HolderDTO.java
@@ -1,11 +1,9 @@
 package com.bank_accounts.domain.dto;
 
-import java.util.List;
 
 public record HolderDTO(
         String firstname,
         String lastname,
-        String ssn,
-        List<AccountDTO> accounts
+        String ssn
 ) {
 }
--- src/main/java/com/bank_accounts/domain/mappers/HolderDTOMapper.java
+++ src/main/java/com/bank_accounts/domain/mappers/HolderDTOMapper.java
@@ -5,27 +5,16 @@ import com.bank_accounts.domain.entities.Holder;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 public class HolderDTOMapper implements Function<Holder, HolderDTO> {
-
-    private final AccountDTOMapper accountDTOMapper;
-
-    public HolderDTOMapper(AccountDTOMapper accountDTOMapper) {
-        this.accountDTOMapper = accountDTOMapper;
-    }
 
     @Override
     public HolderDTO apply(Holder holder) {
         return new HolderDTO(
                 holder.getFirstname(),
                 holder.getLastname(),
-                holder.getSsn(),
-                holder.getAccounts()
-                        .stream()
-                        .map(accountDTOMapper)
-                        .collect(Collectors.toList())
+                holder.getSsn()
         );
     }
 }
